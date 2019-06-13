e9bbb20611c3e7a879de7ed995ee96dab6418f93
ZehuaWang <transforminottawa@gmail.com>
2019-06-13 09:51:47 -0400

    Add example for async and await

---
+++ ES6/async.js
@@ -0,0 +1,15 @@
+function resolveAfter2Seconds() {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve('resolved');
+		}, 2000);
+	});
+}
+
+async function asyncCall() {
+	console.log('calling');
+	var result = await resolveAfter2Seconds();
+	console.log(result);
+}
+
+asyncCall();
