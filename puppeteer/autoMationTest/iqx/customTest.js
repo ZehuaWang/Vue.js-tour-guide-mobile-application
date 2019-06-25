2ef55e844d725c6fb3f9d0544d9c0dbafc78fef5
ZehuaWang <transforminottawa@gmail.com>
2019-06-25 15:08:28 -0400

    Add the paganationCrawler

--- puppeteer/autoMationTest/iqx/customTest.js
+++ puppeteer/autoMationTest/iqx/customTest.js
@@ -1,2 +1,8 @@
 var createQuote = require("./createQuote");
-createQuote();
+var generateProposal = require("./generateProposalByQuote");
+
+// Quote 1628 can not generate proposal -> why
+var quoteArr = [1807,1620];
+(async() =>{
+    await quoteArr.forEach((quote) =>{ generateProposal(quote)});
+})();
