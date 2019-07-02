b3ee10156fb8aa65250f72a50ffc8a96943ce253
Zehua Wang <transforminottawa@gmail.com>
2019-07-01 21:10:06 -0400

    Use a list to store the swipeer list

--- travelquora/src/pages/home/components/Swiper.vue
+++ travelquora/src/pages/home/components/Swiper.vue
@@ -1,33 +1,47 @@
 <template>
-
-    <swiper :options="swiperOption">
-    <!-- slides -->
-    <swiper-slide>
-        <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/7ea90b7bd72642e834ced3953e231e5e.jpg_750x200_b0c66e21.jpg">
-    </swiper-slide>
-    <swiper-slide>
-        <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/e02cceb2dbbfcd2871cdb7531cf311f1.jpg_750x200_63d3d88b.jpg">
-    </swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-    </swiper>
-
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'HomeSwiper',
-        data() {
+        data () {
             return {
-                swiperOption: {}
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [{
+                    id: '0001',
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/7ea90b7bd72642e834ced3953e231e5e.jpg_750x200_b0c66e21.jpg'
+                },{
+                    id: '0002', 
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/e02cceb2dbbfcd2871cdb7531cf311f1.jpg_750x200_63d3d88b.jpg'
+                }]
             }
         }
     }
 </script>
 
 <style lang="stylus" scoped>
-
-    .swiper-img
-        width: 100%
-
+    
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important 
+    
+    .wrapper
+        overflow: hidden
+        width : 100%
+        height: 0
+        padding-bottom: 31.25%
+        .swiper-img
+            width: 100%
 </style>
