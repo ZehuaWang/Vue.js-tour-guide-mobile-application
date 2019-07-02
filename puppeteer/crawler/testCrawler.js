9f78639af8efbbb2937b4b21248685f484058784
ZehuaWang <transforminottawa@gmail.com>
2019-07-02 09:33:36 -0400

    change the test crawler

--- puppeteer/crawler/testCrawler.js
+++ puppeteer/crawler/testCrawler.js
@@ -2,4 +2,4 @@ const schedule = require('node-schedule');
 
 var getNews = require('./oinpCrawler');
 
-var j = schedule.scheduleJob('43 8 * * *', ()=>{getNews()});
+var j = schedule.scheduleJob('00 08 * * *', ()=>{getNews()});
