7aa2728b8927587bbb4a8f91fa2e524126f217dd
Zehua Wang <transforminottawa@gmail.com>
2019-07-01 22:25:38 -0400

    Finishe the layout of icons

--- travelquora/src/pages/home/components/Icons.vue
+++ travelquora/src/pages/home/components/Icons.vue
@@ -1,6 +1,55 @@
 <template>
     <div class="icons">
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+        </div>                                                
     </div>
+    
 </template>
 
 <script>
@@ -9,10 +58,41 @@
     }
 </script>
 
-<style lang="stylus" scoped> 
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
     .icons
+        width: 100%
         overflow: hidden
         height: 0
         padding-bottom: 50%
-        background : green
+        
+        .icon
+            position : relative
+            float: left
+            width: 25%
+            height: 0
+            padding-bottom: 25%
+            
+            .icon-img
+                position : absolute
+                top: 0
+                left: 0
+                right: 0
+                bottom: .44rem
+                box-sizing: border-box
+                padding : .1rem
+                
+                .icon-img-content
+                    display : block
+                    margin : 0 auto
+                    height: 100%
+            .icon-desc
+                position: absolute
+                left: 0
+                right: 0
+                bottom: 0
+                height: .44rem
+                text-align: center
+                line-height : .44rem
+                color: $darkTextColor
 </style>
