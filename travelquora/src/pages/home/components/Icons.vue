9a38bcc9b0d288e0ef7e2cef937b3f06c3cbf716
Zehua Wang <transforminottawa@gmail.com>
2019-07-02 20:41:05 -0400

    Use the computed attr to do the paganation

--- travelquora/src/pages/home/components/Icons.vue
+++ travelquora/src/pages/home/components/Icons.vue
@@ -1,20 +1,12 @@
 <template>
     <div class="icons">
         <swiper>
-            <swiper-slide>    
-                <div class="icon">
+            <swiper-slide v-for="(page,index) of pages" :key="index">    
+                <div class="icon" v-for="item of page" :key="item.id">
                     <div class="icon-img">
-                        <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                        <img class='icon-img-content' :src='item.imgUrl'>
                     </div>
-                    <p class="icon-desc">热门景点</p>
-                </div>
-            </swiper-slide>
-            <swiper-slide>    
-                <div class="icon">
-                    <div class="icon-img">
-                        <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-                    </div>
-                    <p class="icon-desc">热门景点</p>
+                    <p class="icon-desc">{{item.desc}}</p>
                 </div>
             </swiper-slide>
         </swiper>                                               
@@ -23,7 +15,34 @@
 
 <script>
     export default {
-        name: 'HomeIcons'
+        name: 'HomeIcons',
+        data() {
+            return {
+                iconList: [{id:'0001',imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',desc:'热门景点'},
+                {id:'0002',imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',desc:'北京故宫'},
+                {id:'0003',imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',desc:'北京必游'},
+                {id:'0004',imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',desc:'水上乐园'},
+                {id:'0005',imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/0334cf5430b9b5505fd79e2b8d7e8670.png',desc:'长城游览'},
+                {id:'0006',imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/338c5b924c5809e8c7b14f60a953c3e2.png',desc:'欢乐谷'},
+                {id:'0007',imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',desc:'动植物园'},
+                {id:'0008',imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/fa/2548667cb6e902.png',desc:'野生动物'},
+                {id:'0009',imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/f5/c4c9d9830404e602.png',desc:'室内娱乐'}
+                ]
+            }
+        },
+        computed:{
+            pages() {
+                const pages = []
+                this.iconList.forEach((item,index) => {
+                    const page = Math.floor(index / 8)
+                    if(!pages[page]) {
+                        pages[page] = []
+                    }
+                    pages[page].push(item)
+                })
+                return pages
+            } 
+        }
     }
 </script>
 
