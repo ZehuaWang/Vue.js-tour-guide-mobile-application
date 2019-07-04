e9417d56c19ae4340bda25618e7f2f8265e0825a
Zehua Wang <transforminottawa@gmail.com>
2019-07-03 22:19:29 -0400

    Add the header for city selection page

--- travelquora/src/pages/home/components/Header.vue
+++ travelquora/src/pages/home/components/Header.vue
@@ -7,9 +7,11 @@
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
         </div>
-        <div class="header-right">{{this.city}} 
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to='/city'>
+            <div class="header-right">{{this.city}} 
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -26,7 +28,7 @@
     @import '~styles/varibles.styl'
     .header
         display : flex
-        line-height : .86rem
+        line-height : $headerHeight
         background: $bgColor
         color : #fff
         .header-left
@@ -49,6 +51,7 @@
             width : 1.24rem
             float : right
             text-align: center
+            color: #fff
             .arrow-icon
                 margin-left: -.04rem
                 font-size : .24rem
