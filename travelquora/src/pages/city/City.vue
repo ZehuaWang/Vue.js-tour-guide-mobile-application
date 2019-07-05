ee5d2007c4c8e0ed6ce4b655ec29e9f250265481
Zehua Wang <transforminottawa@gmail.com>
2019-07-04 21:17:59 -0400

    Finishe the search module

--- travelquora/src/pages/city/City.vue
+++ travelquora/src/pages/city/City.vue
@@ -1,18 +1,24 @@
 <template>
-    <city-header></city-header>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
 </template>
 
-<<script>
+<script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
     name: 'City',
     components: {
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
     
 <style lang="stylus" scoped>
+
 
 </style>
 
