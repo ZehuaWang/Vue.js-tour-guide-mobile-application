160d50d7287112d2bdcd05a19a0b019f16ef5deb
Zehua Wang <transforminottawa@gmail.com>
2019-07-09 20:56:54 -0400

    Set the page overflow hidden

--- travelquora/src/pages/city/components/List.vue
+++ travelquora/src/pages/city/components/List.vue
@@ -19,10 +19,43 @@
         </div>
         <div class="area">
             <div class="title border-topbottom">热门城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>                                                
+            </div>
+        </div>        
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">
+                    阿拉尔
+                </div>
+                <div class="item border-bottom">
+                    阿拉尔
+                </div>                
+            </div>
         </div>
         <div class="area">
             <div class="title border-topbottom">A</div>
-        </div>        
+            <div class="item-list">
+                <div class="item border-bottom">
+                    阿拉尔
+                </div>
+                <div class="item border-bottom">
+                    阿拉尔
+                </div>                
+            </div>
+        </div>                 
     </div>
 </template>
 
@@ -40,22 +73,38 @@ export default {
             border-color : #ccc
         &:after
             border-color : #ccc
-    .title
-        line-height: .44rem
-        background: #eee
-        padidng-left: .2rem
-        color : #666
-        font-size: .26rem
-    .button-list
+    .border-bottom
+        &:before
+            border-color : #ccc
+    .list
         overflow : hidden
-        padding: .1rem .6rem .1rem .1rem
-        .button-wrapper
-            float: left
-            width: 33.33%
-            .button
-                margin : .1rem
-                padding : .1rem 0
-                text-align: center
-                border: .02rem solid #ccc
-                border-radius: .06rem
+        position : absolute
+        top: 1.58rem
+        left : 0
+        right: 0
+        bottom : 0   
+        .title
+            line-height: .44rem
+            background: #eee
+            padding : 0 0 0 .2rem 
+            color : #666
+            font-size: .26rem
+        .button-list
+            overflow : hidden
+            padding: .1rem .6rem .1rem .1rem
+            .button-wrapper
+                float: left
+                width: 33.33%
+                .button
+                    margin : .1rem
+                    padding : .1rem 0
+                    text-align: center
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+        .item-list
+            .item
+                line-height : .76rem
+                color: #666
+                padding-left: .2rem
+
 </style>
