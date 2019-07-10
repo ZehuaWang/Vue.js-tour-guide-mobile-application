37ce21e144bca067d5d99b814ddfa22a229ee30c
Zehua Wang <transforminottawa@gmail.com>
2019-07-09 20:16:24 -0400

    Adjust the layout of city list

--- travelquora/src/pages/city/components/List.vue
+++ travelquora/src/pages/city/components/List.vue
@@ -6,6 +6,15 @@
                 <div class="button-wrapper">
                     <div class="button">北京</div>
                 </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>                                                
             </div>
         </div>
         <div class="area">
@@ -38,8 +47,15 @@ export default {
         color : #666
         font-size: .26rem
     .button-list
-        padding: .1rem
-        .button
+        overflow : hidden
+        padding: .1rem .6rem .1rem .1rem
+        .button-wrapper
             float: left
             width: 33.33%
+            .button
+                margin : .1rem
+                padding : .1rem 0
+                text-align: center
+                border: .02rem solid #ccc
+                border-radius: .06rem
 </style>
