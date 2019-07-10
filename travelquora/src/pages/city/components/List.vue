ccf132fcd79902d1d3a8c1dc5a1cd4e1511720ce
Zehua Wang <transforminottawa@gmail.com>
2019-07-10 18:14:40 -0400

    add the alphabet component and css

--- travelquora/src/pages/city/components/List.vue
+++ travelquora/src/pages/city/components/List.vue
@@ -1,68 +1,85 @@
 <template>
-    <div class="list">
-        <div class="area">
-            <div class="title border-topbottom">当前城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>                                                
                 </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>                                                
             </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>                                                
+                </div>
+            </div>        
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>                
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>                
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>                
+                </div>
+            </div>            
         </div>
-        <div class="area">
-            <div class="title border-topbottom">热门城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>                                                
-            </div>
-        </div>        
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">
-                    阿拉尔
-                </div>
-                <div class="item border-bottom">
-                    阿拉尔
-                </div>                
-            </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">
-                    阿拉尔
-                </div>
-                <div class="item border-bottom">
-                    阿拉尔
-                </div>                
-            </div>
-        </div>                 
     </div>
 </template>
 
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name: 'CityList'
+    name: 'CityList',
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.wrapper);
+    }
 }
 </script>
 
@@ -79,12 +96,12 @@ export default {
     .list
         overflow : hidden
         position : absolute
-        top: 1.58rem
+        top: 1.68rem
         left : 0
         right: 0
         bottom : 0   
         .title
-            line-height: .44rem
+            line-height: .54rem
             background: #eee
             padding : 0 0 0 .2rem 
             color : #666
