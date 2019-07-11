71cdced1265c80607ea596d9a6574fe2af5e3f58
ZehuaWang <transforminottawa@gmail.com>
2019-07-11 13:31:44 -0400

    Add an abstract class example

---
+++ designPattern/abStractClass.js
@@ -0,0 +1,17 @@
+//https://stackoverflow.com/questions/29480569/does-ecmascript-6-have-a-convention-for-abstract-classes
+class Abstract {
+    constructor() {
+        if (new.target === Abstract) {
+            throw new TypeError("Can not construct Abstract instances directly");
+        }
+    }
+}
+
+class Derived extends Abstract {
+    constructor() {
+        super();
+    }    
+}
+
+//const a = new Abstract();
+const b = new Derived();
