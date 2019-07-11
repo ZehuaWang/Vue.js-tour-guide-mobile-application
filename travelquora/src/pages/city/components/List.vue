501965c0fdc442666825e34240834a3bb6ad77bf
Zehua Wang <transforminottawa@gmail.com>
2019-07-10 20:40:08 -0400

    Get the alphabet with ajax

--- travelquora/src/pages/city/components/List.vue
+++ travelquora/src/pages/city/components/List.vue
@@ -18,37 +18,16 @@
                     </div>                                                
                 </div>
             </div>        
-            <div class="area">
-                <div class="title border-topbottom">A</div>
+            <div class="area" v-for="(item, key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>                
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbottom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>                
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbottom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>                
+                    <div 
+                    class="item border-bottom"
+                    v-for="innerItem of item"
+                    :key = "innerItem.id"
+                    >
+                    {{innerItem.name}}
+                    </div>              
                 </div>
             </div>            
         </div>
