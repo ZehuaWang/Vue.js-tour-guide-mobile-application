3662b5fbd85c56af5fa4594b43433d37b923156b
Zehua Wang <transforminottawa@gmail.com>
2019-07-10 21:19:03 -0400

    Add a method to locate the content according to the letter

--- travelquora/src/pages/city/components/Alphabet.vue
+++ travelquora/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,13 @@
 <template>
     <ul class="list"> 
-        <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+        <li 
+            class="item" 
+            v-for="(item, key) of cities" 
+            :key="key"
+            @click="handleLetterClick"
+            >
+            {{key}}
+        </li>
     </ul>
 </template>
 
@@ -9,6 +16,11 @@
         name: 'CityAlphabet',
         props: {
             cities: Object
+        },
+        methods: {
+            handleLetterClick (e) {
+                this.$emit('change', e.target.innerText);
+            }
         }
     }
 </script>
