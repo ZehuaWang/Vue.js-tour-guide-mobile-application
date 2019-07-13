185f61d2223bf23b39428b374cea50169ebff040
Zehua Wang <transforminottawa@gmail.com>
2019-07-13 10:31:53 -0400

    Introduce vuex to share the data between different pages

--- TravelQuora/src/main.js
+++ TravelQuora/src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import store from './store'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
@@ -18,6 +20,7 @@ Vue.use(VueAwesomeSwiper)
 new Vue({
     el: '#app',
     router,
+    store,
     components: { App },
     template: '<App/>'
 })
--- travelquora/src/pages/home/components/Header.vue
+++ travelquora/src/pages/home/components/Header.vue
@@ -8,7 +8,7 @@
             输入城市/景点/游玩主题
         </div>
         <router-link to='/city'>
-            <div class="header-right">{{this.city}} 
+            <div class="header-right">{{this.$store.state.city}} 
                 <span class="iconfont arrow-icon">&#xe64a;</span>
             </div>
         </router-link>
@@ -17,10 +17,7 @@
 
 <script>
     export default {   
-        name: 'HomeHeader',
-        props: {
-            city: String
-        }
+        name: 'HomeHeader'
     }
 </script>
 
