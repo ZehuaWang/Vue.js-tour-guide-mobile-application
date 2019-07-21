54107dce56a13c564f6f3f5a8f7e3896f2885fe6
Zehua Wang <transforminottawa@gmail.com>
2019-07-21 10:47:20 -0400

    chang the icon

--- travelquora/src/pages/detail/components/Banner.vue
+++ travelquora/src/pages/detail/components/Banner.vue
@@ -3,7 +3,7 @@
         <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg">
         <div class="banner-info">
             <div class="banner-title">故宫(AAAAA景区)</div>
-            <div class="banner-number"><span class="iconfont">&#xe632;</span>10</div>
+            <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>10</div>
         </div>
     </div>
 </template>
@@ -32,10 +32,16 @@
             color: #fff
             .banner-title
                 flex : 1
+                font-size : .32rem
+                padding: 0 .2rem
             .banner-number
+                margin-top: .14rem
                 padding: 0 .4rem
-                line-height : .4rem
-                height : .4rem
+                line-height : .32rem
+                height : .32rem
                 border-radius: .2rem
                 background: rgba(0,0,0,.8)
+                font-size: .24rem
+                .banner-icon
+                    font-size: .24rem
 </style>
