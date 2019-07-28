56c5c334118d99c992c9ae6108d7791cecbd92d7
Zehua Wang <transforminottawa@gmail.com>
2019-07-28 16:39:12 -0400

    Introduce Swipper on detail page

--- travelquora/src/common/gallary/Gallary.vue
+++ travelquora/src/common/gallary/Gallary.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="container">
         <div class="wrapper">
-            123
-        </div>
+            <swiper>
+            <!-- slides -->
+            <swiper-slide>
+                <img class="gallary-img" src="http://img1.qunarzz.com/wugc/p123/201211/19/a2045d091f02b25493835fbb.png_350x240_cf4cd08e.png">
+            </swiper-slide>
+            <swiper-slide>
+                <img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/1410/9d/fe8109ab5df1c9c324e74284fa802e72.water.jpg_350x240_8571d9ed.jpg">
+            </swiper-slide>            
+            <!-- Optional controls -->
+            <!--<div class="swiper-pagination"  slot="pagination"></div>-->
+            </swiper>
+            </div>
     </div>
 </template>
 
-<<script>
+<script>
 export default {
     name: 'CommonGallary'    
 }
@@ -25,6 +35,10 @@ export default {
         bottom: 0
         background: #000
         .wrapper
-            background: #fff
-            
+            overflow : hidden
+            height: 0
+            width: 100%
+            padding-bottom: 100%
+            .gallary-img
+                width : 100%
 </style>
