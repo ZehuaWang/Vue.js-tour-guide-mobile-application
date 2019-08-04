ae649cb1270aa082eb0de657b223f43af96c1375
Zehua Wang <transforminottawa@gmail.com>
2019-08-04 14:38:29 -0400

    deactivate the handle scroll function from global

--- travelquora/src/pages/detail/components/Header.vue
+++ travelquora/src/pages/detail/components/Header.vue
@@ -44,11 +44,14 @@ export default {
             } else {
                 this.showAbs = true
             }
-            console.log(document.documentElement.scrollTop);
+            //console.log(document.documentElement.scrollTop);
         }
     },
     activated () {
         window.addEventListener('scroll', this.handleScroll)
+    },
+    deactivated () {
+        window.removeEventListener('scroll', this.handleScroll)
     }
 }
 </script>
