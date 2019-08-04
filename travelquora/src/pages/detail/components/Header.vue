5565af810bd5be380ee4d51e514805df6cb3d654
Zehua Wang <transforminottawa@gmail.com>
2019-08-04 14:33:24 -0400

    Finish the header animation logic

--- travelquora/src/pages/detail/components/Header.vue
+++ travelquora/src/pages/detail/components/Header.vue
@@ -1,9 +1,17 @@
 <template>
 <div>    
-    <router-link tag="div" to="/" class="header-abs">
+    <router-link 
+    tag="div" 
+    to="/" 
+    class="header-abs"
+    v-show="showAbs">
         <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
-    <div class="header-fixed">
+    <div 
+    class="header-fixed" 
+    v-show="!showAbs"
+    :style="opacityStyle"
+    >
         <router-link to="/">
             <div class="iconfont header-fixed-back">&#xe624;</div>
         </router-link>
@@ -14,7 +22,34 @@
 
 <script>
 export default {
-    name: 'DetailHeader'
+    name: 'DetailHeader',
+    data() {
+        return {
+            showAbs: true,
+            opacityStyle: {
+                opacity: 0
+            }
+        }
+    },
+    methods: {
+        handleScroll () {
+            const top = document.documentElement.scrollTop;
+            if(top > 60) {
+                let opacity = top / 140;
+                opacity = opacity > 1 ? 1 : opacity;
+                this.opacityStyle = {
+                    opacity
+                };
+                this.showAbs = false;
+            } else {
+                this.showAbs = true
+            }
+            console.log(document.documentElement.scrollTop);
+        }
+    },
+    activated () {
+        window.addEventListener('scroll', this.handleScroll)
+    }
 }
 </script>
 
